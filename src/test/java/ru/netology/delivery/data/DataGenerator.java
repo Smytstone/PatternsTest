c6ca3312163be869d140fe19f3f63535ef21ee54
Alexandr Shchelokov <alex175171@gmail.com>
2024-06-04 12:17:55 +0300

    Added allure

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -4,15 +4,10 @@ import com.github.javafaker.Faker;
 import lombok.Value;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-
-import static java.time.LocalDate.now;
-import static java.time.temporal.ChronoUnit.DAYS;
 
 public class DataGenerator {
     private DataGenerator() {
@@ -21,19 +16,9 @@ public class DataGenerator {
     public static Faker faker = new Faker(new Locale("ru"));
 
     public static String generateDate(int shift) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-//        String startDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-//        String endDate = LocalDate.now().plusYears(5).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        LocalDate start = LocalDate.now();
+        LocalDate start = LocalDate.now().plusDays(4);
         long days = ChronoUnit.DAYS.between(start, LocalDate.now().plusYears(3));
         LocalDate randomDate = start.plusDays(new Random().nextInt((int) days + 1));
-//        LocalDate startDate = LocalDate.now();
-//        LocalDate endDate = LocalDate.of(2025, 10, 31);
-////        LocalDate endDate = LocalDate.now();
-//        long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
-//        long randomNumberOfDays = ThreadLocalRandom.current().nextLong(daysBetween + 1);
-//        LocalDate randomDate = startDate.plusYears(randomNumberOfDays);
         return randomDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
--- src/test/java/ru/netology/delivery/test/PatternsTest.java
+++ src/test/java/ru/netology/delivery/test/PatternsTest.java
@@ -1,19 +1,32 @@
 package ru.netology.delivery.test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.netology.delivery.data.DataGenerator;
 
 import java.time.Duration;
-import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 import static io.opentelemetry.semconv.trace.attributes.SemanticAttributes.FaasDocumentOperationValues.INSERT;
-import static java.time.LocalDate.now;
 import static org.openqa.selenium.Keys.*;
 import static org.openqa.selenium.Keys.BACK_SPACE;
 
 public class PatternsTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @Test
     public void shouldTestForm() {
         var newUser = DataGenerator.Registration.generateUser("ru");
